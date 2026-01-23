ddf8ba4cd451024dcffb55a95dc31a628d37a159
Kavin E <ekavin65@gmail.com>
2026-01-23 15:06:48 +0530

    Largest Number At Least Twice
    Add a method to determine the dominant index in an array.

--- largenum.java
+++ largenum.java
@@ -5,7 +5,6 @@ You are given an integer array nums where the largest integer is unique.
 Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.*/
 
 public class largenum {
-
     public static int dominantIndex(int[] nums) {
 
         int largest = -1;
