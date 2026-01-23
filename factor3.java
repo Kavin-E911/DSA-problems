24e31e9430b7fed34e4529a4bc1881b2013e6c69
Kavin E <ekavin65@gmail.com>
2026-01-23 15:07:27 +0530

    Factors

--- factor3.java
+++ factor3.java
@@ -1,6 +1,4 @@
-
 import java.util.*;
-
 public class factor3 {
     public static void main(String args[] ) throws Exception {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT */
